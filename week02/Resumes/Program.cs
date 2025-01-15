fe6178767178b2b4c194ce3635e0a9f917ab65e0
Jaryne <man24035@byui.edu>
2025-01-16 01:44:09 +0800

    LA: Abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Job job1 = new Job();
+        job1. _jobTitle = "Disney Imagineer";
+        job1. _company = "Walt Disney Imagineering";
+        job1. _startYear = 2022;
+        job1. _endYear = 2025;
+
+        Job job2 = new Job();
+        job2. _jobTitle = "Project Manager";
+        job2. _company = "Amazon";
+        job2. _startYear = 2023;
+        job2. _endYear = 2024;
+
+        Resume myResume = new Resume(); //using the sample.
+        myResume. _name = "James Moriarty";
+
+        myResume. _jobs.Add(job1);
+        myResume. _jobs.Add(job2);
+
+        myResume.Display();
     }
 }
