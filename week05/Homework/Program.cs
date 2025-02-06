f39f7e7928e84c5d94175943751d2f7ed77de646
Jaryne <man24035@byui.edu>
2025-02-07 00:35:51 +0800

    initial commit

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        //Console.WriteLine("Hello World! This is the Homework Project.");
+
+
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        MathAssignment sectionProblem = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "Problems 8-19");
+        WritingAssignment title = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+
+        Console.WriteLine(assignment.GetSummary());
+
+        // Math Assignment
+        Console.WriteLine(sectionProblem.GetSummary());
+        Console.WriteLine(sectionProblem.GetHomeworkList());
+
+        // Writing Assignment
+        Console.WriteLine(title.GetSummary());
+        Console.WriteLine(title.GetWritingInformation());
+
     }
 }
