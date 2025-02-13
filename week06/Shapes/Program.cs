14bef182de1d41c87f7abfb70a095534e2458eab
Jaryne <man24035@byui.edu>
2025-02-13 22:02:46 +0800

    initial commit

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("red", 5));
+        shapes.Add(new Rectangle("blue", 4, 6));
+        shapes.Add(new Circle("green", 3));
+
+        foreach (var shape in shapes)
+        {
+            Console.WriteLine($"The {shape.GetColor()} shape has an area of {shape.GetArea():F2}.");
+        }
     }
 }
