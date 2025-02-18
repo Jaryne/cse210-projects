96a924fff3c05026c58d1123c9fa409c855e17cb
Jaryne <man24035@byui.edu>
2025-02-18 21:35:58 +0800

    initial commit

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        // Last assignment!
+
+        List<Activity> activities = new List<Activity>
+        {
+            new Running(new DateTime(2022, 11, 3), 30, 5),
+            new Cycling(new DateTime(2022, 11, 3), 30, 20),
+            new Swimming(new DateTime(2022, 11, 3), 30, 30)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
